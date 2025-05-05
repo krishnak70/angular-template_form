baef8ada46434a0f238a2528ce471e0e9fd00433
krishnak70 <krishna.pratap@mymobioffice.com>
2025-05-05 17:13:03 +0530

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Template_from';
+
+  constructor() {
+  }
+  ngOnInit(): void {
+  }
+
+  userdata={
+    username:'',
+    password:'',
+  };
+  onSubmit(){
+    localStorage.setItem('userdata',JSON.stringify(this.userdata));
+    console.log(this.userdata);
+  }
 }
